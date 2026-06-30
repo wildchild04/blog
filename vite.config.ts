e5adc649bfa3026eece78b6a93fcb3485a72091d
wildchild-w <gustavo.romerorz@gmail.com>
2026-06-30 11:41:19 -0600

    vite update 8.1.2

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173,
+    strictPort: true,
+    host: "127.0.0.1",
+  },
   define: {
     "process.env.NODE_ENV": JSON.stringify("production"),
   },
